cbcf2ac54367d2122ce73adfb6e454f09b4ecd0b
Peter Saxton <peterhsaxton@gmail.com>
2014-11-30 20:11:33 +0000

    test string method raises errors

--- lib/claudia.rb
+++ lib/claudia.rb
@@ -2,7 +2,25 @@ require "claudia/version"
 require 'delegate'
 
 module Claudia
-  class Presenter < SimpleDelegator
+	StringMethodError = Class.new(StandardError)
+  class Presenter
+  	def initialize(base_object)
+  		@base_object = base_object
+  	end
+
+  	def __getobj__
+  		@base_object
+  	end
+
+  	def method_missing(method_name, *arguments, &block)
+  		if __getobj__.respond_to?(method_name)
+  			result = __getobj__.public_send method_name
+  			raise StringMethodError.new unless result.class == String
+  			result
+  		else
+  			super
+  		end
+  	end
   	
   end
 end
--- test/presenter/presenter_test.rb
+++ test/presenter/presenter_test.rb
@@ -2,6 +2,7 @@ require_relative '../test_config'
 
 class PresenterTest < MiniTest::Test
 	StringStruct = Struct.new(:get_string)
+	NumberStruct = Struct.new(:get_number)
 
 	def string_example
 		StringStruct.new 'testy string'
@@ -10,5 +11,13 @@ class PresenterTest < MiniTest::Test
 	def test_presenter_delegates_string_methods
 		presenter = Claudia::Presenter.new string_example
 		assert_equal 'testy string', presenter.get_string
+	end
+
+	def test_presenter_errors_for_invalid_string_method
+		object = NumberStruct.new 5
+		presenter = Claudia::Presenter.new object
+		assert_raises Claudia::StringMethodError do
+			puts presenter.get_number
+		end
 	end
 end
